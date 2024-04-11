e33ed57c0867f0ae9751977cd0903039e4acbf7d
Nipatsku <niilo.keinanen@hotmail.com>
2024-04-11 18:11:22 +0300

    Fix invite url

--- config/database.js
+++ config/database.js
@@ -1,5 +1,10 @@
+// module.exports = {
+//   address: "192.168.1.169:3000",
+//   database: "mongodb://127.0.0.1:27017/niilorganize",
+//   secret: 'ascd3qda3a3fc213123da3d3aw3daw3a321312367869568f'
+// };
 module.exports = {
-  address: "https://niilorganize-76544a0e5363.herokuapp.com/",
+  address: "https://niilorganize-76544a0e5363.herokuapp.com",
   database: `${process.env.MONGODB_URI}/niilorganize`,
   secret: process.env.SECRET
 };
